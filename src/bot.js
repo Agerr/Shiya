9422763fff616ba3f7af0868e2da7c56dfd5a656
Agerr <augustasm2006@gmail.com>
2022-01-12 20:55:56 +0200

    Basic bot

---
+++ src/bot.js
@@ -0,0 +1,17 @@
+const Discord = require(`discord.js`);
+
+const bot = new Discord.Client({ intents: 6095 });
+
+const config = require(`./config.json`);
+
+console.log(`\nNode.js ${process.version}\nDiscord.js v${Discord.version}`);
+
+bot.on("ready", () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+})
+
+bot.on("messageCreate", message => {
+    if(message.content === "ping") return message.channel.send("pong lmao");
+})
+
+bot.login(config.token);
