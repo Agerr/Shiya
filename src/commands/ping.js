6832ed93781524f652787ecbee2fad0750b8fdfd
Eliiia <grzesiul1995@gmail.com>
2022-01-12 20:37:49 +0000

    Added ping.js command

---
+++ src/commands/ping.js
@@ -0,0 +1,8 @@
+module.exports.run = (bot, message, args) => {
+    message.channel.send(`My ping is \`${bot.ws.ping}ms\``)
+}
+
+module.exports.info = {
+    "name": "ping",
+    "description:": "Sends the ping of the bot back!"
+}
