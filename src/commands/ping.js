cad8db2356928e1fcc36208c681c4d7f516637fe
Agerr <augustasm2006@gmail.com>
2022-01-14 14:32:15 +0200

    Added config as parameter you would pass to every module

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,4 +1,4 @@
-module.exports.run = (bot, message, args) => {
+module.exports.run = (bot, message, args, config) => {
     message.channel.send('Calculating ping...').then((resultMessage) => {
         const ping = resultMessage.createdTimestamp - message.createdTimestamp;
         resultMessage.edit({ content: `Bot latency: \`${ping}ms\`, API latency: \`${bot.ws.ping}ms\`.` });
