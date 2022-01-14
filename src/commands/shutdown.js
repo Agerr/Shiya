006318e811c692fa390f49517b7520cf3ce58347
Agerr <augustasm2006@gmail.com>
2022-01-14 14:49:38 +0200

    Fixed abit shutdown idk how reverts work lol sr

--- src/commands/shutdown.js
+++ src/commands/shutdown.js
@@ -3,6 +3,7 @@ module.exports.run = (bot, message, args, config) => {
     const timeDate = `${date.getUTCFullYear()}/${date.getUTCMonth() + 1}/${date.getUTCDate()} @ ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()} UTC`;
 
     console.log(`Process killed by ${message.author.name} ${timeDate}`);
+    process.exit();
 }
 
 module.exports.info = {
