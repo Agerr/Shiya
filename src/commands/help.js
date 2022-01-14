d39d525171331cd6a825e3518a1bdd48d62fb3cd
Agerr <augustasm2006@gmail.com>
2022-01-14 21:22:15 +0200

    help command optimization

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,14 +2,16 @@ const Discord = require(`discord.js`);
 const config = require(`../config.json`);
 const fs = require("fs");
 
-let moderationCommands = ``;
-let informationCommands = ``;
-let gamesCommands = ``;
-let imagesCommands = ``;
-let funCommands = ``;
-let randomCommands = ``;
-let utilityCommands = ``;
-let developerCommands = ``;
+const commands = {
+    "moderation": "",
+    "information": "",
+    "games": "",
+    "images": "",
+    "fun": "",
+    "random": "",
+    "utility": "",
+    "developer": ""
+}
 
 fs.readdir("./commands/", (error, files) => {
     if(error) throw `Error reading commands: ${error}`;
@@ -19,64 +21,60 @@ fs.readdir("./commands/", (error, files) => {
 
         const command = require(`./${file}`);
         
-        switch (command.info.category){
+        switch (command.info.category) {
             case 'moderation':
-                moderationCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.moderation += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'information':
-                informationCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.information += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'games':
-                gamesCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.games += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'images':
-                imagesCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.images += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'fun':
-                funCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.fun += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'random':
-                randomCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.random += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'utility':
-                utilityCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.utility += ` \`\`${command.info.name}\`\`,`;
             break;
 
             case 'developer':
-                developerCommands += ` \`\`${command.info.name}\`\`,`;
+                commands.developer += ` \`\`${command.info.name}\`\`,`;
             break;
         }
     });
-    moderationCommands = moderationCommands != `` ? moderationCommands.substring(0, moderationCommands.length - 1) + ` ` : `None`;
-    informationCommands = informationCommands != `` ? informationCommands.substring(0, informationCommands.length - 1) + ` ` : `None`;
-    gamesCommands = gamesCommands != `` ? gamesCommands.substring(0, gamesCommands.length - 1) + ` ` : `None`;
-    imagesCommands = imagesCommands != `` ? imagesCommands.substring(0, imagesCommands.length - 1) + ` ` : `None`;
-    funCommands = funCommands != `` ? funCommands.substring(0, funCommands.length - 1) + `.` : `None`;
-    randomCommands = randomCommands != `` ? randomCommands.substring(0, randomCommands.length - 1) + ` ` : `None`;
-    utilityCommands = utilityCommands != `` ? utilityCommands.substring(0, utilityCommands.length - 1) + ` ` : `None`;
-    developerCommands = developerCommands != `` ? developerCommands.substring(0, developerCommands.length - 1) + ` ` : `None`;
+    
+    for (const category in commands) {
+        commands[category] = commands[category] != `` ? commands[category].substring(0, commands[category].length - 1) + ` ` : `None`;
+    };
 });
 
 module.exports.run = (bot, message, args) => {
     const helpEmbed = new Discord.MessageEmbed()
-    .setAuthor({ name:  bot.user.username + ` Commands List`, iconURL: bot.user.displayAvatarURL() })
-    .setColor(config.color)
-    .addFields(
-        { name: `Moderation`, value: `${moderationCommands}` },
-        { name: `Information`, value: `${informationCommands}` },
-        { name: `Games`, value: `${gamesCommands}` },
-        { name: `Images`, value: `${imagesCommands}` },
-        { name: `Fun`, value: `${funCommands}` },
-        { name: `Random`, value:  `${randomCommands}`},
-        { name: `Utility`, value: `${utilityCommands}` },
-        { name: `Developer`, value: `${developerCommands}` },
-    );
+        .setAuthor({ name:  bot.user.username + ` Commands List`, iconURL: bot.user.displayAvatarURL() })
+        .setColor(config.color)
+        .addFields(
+            { name: `Moderation`, value: `${commands.moderation}` },
+            { name: `Information`, value: `${commands.information}` },
+            { name: `Games`, value: `${commands.games}` },
+            { name: `Images`, value: `${commands.images}` },
+            { name: `Fun`, value: `${commands.fun}` },
+            { name: `Random`, value:  `${commands.fun}`},
+            { name: `Utility`, value: `${commands.utility}` },
+            { name: `Developer`, value: `${commands.developer}` },
+        );
 
     message.channel.send({ embeds: [helpEmbed] });
 }
