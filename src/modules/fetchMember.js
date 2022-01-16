e173c5f88b5bae551b5eb00f7319dcbedde7f483
Agerr <augustasm2006@gmail.com>
2022-01-16 21:08:30 +0200

    Fixed issue with fetching member from diff server

--- src/modules/fetchMember.js
+++ src/modules/fetchMember.js
@@ -10,7 +10,7 @@ module.exports = async (message, bot) => {
         } else if (/^(0|([1-9]\d*))$/.test(args[1])) {
             target = await message.guild.members.cache.get(args[1]);
         } else {
-            target = await message.guild.members.cache.get(await bot.users.cache.find(user => user.username.toLowerCase() == args[1].toLowerCase()).id);
+            target = await message.guild.members.cache.get((await message.guild.members.cache.find(member => member.user.username.toLowerCase() == args[1].toLowerCase())).user.id);
         }
     } catch (error) {
         target = false;
