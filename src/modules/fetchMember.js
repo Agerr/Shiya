6bca749225a7178702a14bf8da44c3590d3c956b
Agerr <augustasm2006@gmail.com>
2022-01-18 17:36:00 +0200

    +member fetching options: tag, name. multiple people share name=complain

--- src/modules/fetchMember.js
+++ src/modules/fetchMember.js
@@ -1,20 +1,49 @@
-const config = require(`../config.json`);
+const Discord = require(`discord.js`),
+      config = require(`../config.json`);
 
 module.exports = async (message, bot) => {
     const args = message.content.substring(config.prefix.length).split(' ');
     let target;
 
-    try {
-        if (message.mentions.users.first()) {
-            target = await message.guild.members.cache.get(await message.mentions.users.first().id);
-        } else if (/^(0|([1-9]\d*))$/.test(args[1])) {
-            target = await message.guild.members.cache.get(args[1]);
-        } else {
-            target = await message.guild.members.cache.get((await message.guild.members.cache.find(member => member.user.username.toLowerCase() == args[1].toLowerCase())).user.id);
+    if (args[1]){
+        try {
+            if (message.mentions.users.first()) {
+                target = await message.guild.members.cache.get(await message.mentions.users.first().id);
+            } else if (/^(0|([1-9]\d*))$/.test(args[1])) {
+                target = await message.guild.members.cache.get(args[1]);
+            } else {
+                target = await message.guild.members.cache.find(member => member.user.tag.toLowerCase() == args[1].toLowerCase())
+                if (target == undefined) {
+                    let membersArray = Array.from(await message.guild.members.cache.filter(member => member.user.username.toLowerCase() == args[1].toLowerCase()).values());
+
+                    if (membersArray.length == 1) {
+                        target = await message.guild.members.cache.get((membersArray[0].user.id));
+                    } else if (membersArray.length > 1){
+                        let usersList = ``;
+                        membersArray.forEach(element => {
+                            usersList += `${element.user.tag}\n`;
+                        })
+    
+                        embed = new Discord.MessageEmbed()
+                            .setColor(config.color)
+                            .setTitle(`Multiple users have this name:`)
+                            .setDescription(`${usersList}`);
+    
+                        message.channel.send({ embeds: [embed] });
+                    }
+                }
+            }
+        } catch (error) {
+            target = false;
+            message.channel.send({ content: `There was an error while fetching member.` });
+            console.log(error);
         }
-    } catch (error) {
-        target = false;
-        message.channel.send({ content: `Couldn't fetch member.` });
     }
+
+    if (target == undefined) { 
+        target = false;
+        message.channel.send({ content: `Could't find this member.` });
+    }
+
     return target;
 }
