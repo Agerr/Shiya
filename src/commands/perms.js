e995bbb7d9adf7d87ef6152cdeda97e5c03f88e1
Agerr <augustasm2006@gmail.com>
2022-01-24 16:36:14 +0200

    Added perms module

---
+++ src/commands/perms.js
@@ -0,0 +1,12 @@
+module.exports.run = (bot, message, args) => {
+    
+}
+
+module.exports.info = {
+    "name": "perms",
+    "description": "Sends users permissions",
+    "usage": "perms [mention | id | name | tag]",
+    "alias": "none",
+    "category": "information",
+    "perm": "public"
+}
