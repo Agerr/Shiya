ee0eac3c2a92b401a11eecd138d7ffb561b0a037
Agerr <augustasm2006@gmail.com>
2022-01-26 17:40:29 +0200

    Slowmode hotfix

--- src/commands/slowmode.js
+++ src/commands/slowmode.js
@@ -2,6 +2,7 @@ module.exports.run = async (bot, message, args) => {
     if (!message.member.permissions.has(`MANAGE_CHANNELS`)) return message.channel.send({ content: `The \`${args[0]}\` command requires "Manage channels" permission. `});
 
     if (!/^0|([1-9]\d*)$/.test(args[1])) return message.channel.send({ content: `Invalid amount.` });
+    if (parseInt(args[1]) > 21600) return message.channel.send({ content: `Value should be less or equal to 21600.` });
 
     await message.channel.setRateLimitPerUser(parseInt(args[1]));
     await message.channel.send({ content: `Slowmode has been set to ${args[1]} seconds.` });
