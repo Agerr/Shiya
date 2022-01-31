52f7c5e22d74561b999258dc804767d5a99f1f09
Agerr <augustasm2006@gmail.com>
2022-01-31 14:59:56 +0200

    Updated regex

--- src/modules/timeConvert.js
+++ src/modules/timeConvert.js
@@ -2,11 +2,11 @@ module.exports = async (string) => {
 
     const coefficient = await parseInt(string.replace(/[^0-9,.]/g, ""))
     let seconds;
-    if (/^0|([1-9]\d*)d$/.test(string) || /^0|([1-9]\d*)day$/.test(string) | /^0|([1-9]\d*)days$/.test(string)) {
+    if (/^[1-9]\d*(\.\d+)?d$/.test(string) || /^[1-9]\d*(\.\d+)?day$/.test(string) | /^[1-9]\d*(\.\d+)?days$/.test(string)) {
         seconds = coefficient * 86400;
-    } else if (/^0|([1-9]\d*)h$/.test(string) || /^0|([1-9]\d*)hour$/.test(string) || /^0|([1-9]\d*)hours$/.test(string)) {
+    } else if (/^[1-9]\d*(\.\d+)?h$/.test(string) || /^[1-9]\d*(\.\d+)?hour$/.test(string) || /^[1-9]\d*(\.\d+)?hours$/.test(string)) {
         seconds = coefficient * 3600;
-    } else if (/^0|([1-9]\d*)m$/.test(string) || /^0|([1-9]\d*)min$/.test(string) || /^0|([1-9]\d*)mins$/.test(string) || /^0|([1-9]\d*)minute$/.test(string) || /^0|([1-9]\d*)minutes$/.test(string)) {
+    } else if (/^[1-9]\d*(\.\d+)?m$/.test(string) || /^[1-9]\d*(\.\d+)?min$/.test(string) || /^[1-9]\d*(\.\d+)?mins$/.test(string) || /^[1-9]\d*(\.\d+)?minute$/.test(string) || /^[1-9]\d*(\.\d+)?minutes$/.test(string)) {
         seconds = coefficient * 60;
     } else {
         seconds = coefficient;
@@ -15,6 +15,6 @@ module.exports = async (string) => {
     Math.round(seconds);
     
     if (!/^([1-9]\d*)$/.test(seconds)) seconds = false;
-    
+
     return seconds;
 }
